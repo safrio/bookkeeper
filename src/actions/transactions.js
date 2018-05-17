06302028813a2c676a7b810aaa9b98a6836bd83d
Ivan Safonov <safrio@gmail.com>
2018-05-17 16:41:10 +0500

    Actions & Config

---
+++ src/actions/transactions.js
@@ -0,0 +1,42 @@
+import config from '../config/config'
+
+export function transactionsHasErrored(bool) {
+  return {
+    type: 'TRANSACTIONS_HAS_ERRORED',
+    transactionsHasErrored: bool
+  };
+}
+
+export function transactionsIsLoading(bool) {
+  return {
+    type: 'TRANSACTIONS_IS_LOADING',
+    transactionsIsLoading: bool
+  };
+}
+
+export function transactionsFetchDataSuccess(transactions) {
+  return {
+    type: 'TRANSACTIONS_FETCH_DATA_SUCCESS',
+    transactions
+  };
+}
+
+export function transactionsFetchData() {
+  return (dispatch) => {
+    dispatch(transactionsIsLoading(true));
+
+    fetch("${config.fetch_transactions_url}?from=2011-11-11&to=2022-11-11")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        dispatch(transactionsIsLoading(false));
+
+        return response;
+      })
+      .then((response) => response.json())
+      .then((transactions) => dispatch(transactionsFetchDataSuccess(transactions)))
+      .catch(() => dispatch(transactionsHasErrored(true)));
+  };
+}
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export default config = {
+	fetch_transactions_url: 'http://localhost:3000/api/v1/transactions',
+}
