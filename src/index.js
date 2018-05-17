754757bba5a2d64dbcf72d9d9c22ea21f93e421b
Ivan Safonov <safrio@gmail.com>
2018-05-17 16:30:22 +0500

    Redux & Thunk & Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import App from './App';
+import './index.css';
 
 render(
 	<App />,
